5a1a15a401348bebab259b592840d4af74616970
Mmmu <1098335635@qq.com>
2015-11-24 12:02:42 +0800

    set up

---
+++ IntroductionToJavaPromgraming_ComprehensiveVersion_10thEdition/Chapter01/PE01.java
@@ -0,0 +1,7 @@
+class PE01 {
+	public static void main(String[] args) {
+		System.out.println("Welcome to Java.");
+		System.out.println("Welcome to Computer science.");
+		System.out.println("Promgraming is fun.");
+	}
+}
---
+++ IntroductionToJavaPromgraming_ComprehensiveVersion_10thEdition/Chapter01/PE04.java
@@ -0,0 +1,9 @@
+class PE04 {
+	public static void main(String[] args) {
+		System.out.println("a\ta^2\ta^3");
+		System.out.println(1 + "\t" + (1*1) + "\t" + (1*1*1));
+		System.out.println(2 + "\t" + (2*2) + "\t" + (2*2*2));
+		System.out.println(3 + "\t" + (3*3) + "\t" + (3*3*3));
+		System.out.println(4 + "\t" + (4*4) + "\t" + (4*4*4));
+	}
+}
