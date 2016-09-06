c3bcbd1147158a7aca2831c52b484d03abfef36f
Kuuo <Kuuo@users.noreply.github.com>
2016-09-06 15:31:17 +0800

    Delete CH0204.cs

--- CSharp_Programming_And_Applications_2ed/Chapter02/CH0204.cs
+++
@@ -1,9 +0,0 @@
-using System;
-
-class MainClass {
-    static void Main() {
-        Console.WriteLine("{0}--{0:p}good", 12.34F);
-        Console.WriteLine("{0}--{0:####}good", 0);
-        Console.WriteLine("{0}--{0:00000}good", 456);
-    }
-}
