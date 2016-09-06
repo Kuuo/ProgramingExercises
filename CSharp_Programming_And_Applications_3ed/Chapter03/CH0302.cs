27f8c4dbab97c3d249e1ba52ce9abd7e82c27bbb
Kuuo <Kuuo@users.noreply.github.com>
2016-09-06 15:52:23 +0800

    update to CH03 #03

---
+++ CSharp_Programming_And_Applications_3ed/Chapter03/CH0302.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace CH03 {
+    class CH0302 {
+        static void Main(string[] args) {
+            MyClass mc1 = new MyClass();
+            MyClass mc2 = new MyClass("This is a string.");
+            MyClass[] mcarr = new MyClass[5];
+        }
+    }
+
+    class MyClass {
+        public MyClass() {
+            Console.WriteLine("MyClass");
+        }
+
+        public MyClass(string str) {
+            Console.WriteLine(str);
+        }
+    }
+}
---
+++ CSharp_Programming_And_Applications_3ed/Chapter03/CH0303.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CH03 {
+    class CH0303 {
+        static void Main(string[] args) {
+            A a = new A();
+            Console.WriteLine(a.MyMethod(1));
+            B b = new B();
+            Console.WriteLine(b.MyMethod(1));
+        }
+    }
+
+    class A {
+        public virtual int MyMethod(int arg) {
+            return arg + 10;
+        }
+    }
+
+    class B : A {
+        public override int MyMethod(int arg) {
+            return arg + 50;
+        }
+        
+    }
+}
